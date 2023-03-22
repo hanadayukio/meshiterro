38bef7e354facee7f0d8df9ab02f616925652c3f
EC2 Default User <ec2-user@ip-172-31-19-177.ap-northeast-1.compute.internal>
2023-03-22 14:39:18 +0000

    【finish】24章

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,14 @@ class PostImage < ApplicationRecord
     
     has_one_attached :image
     belongs_to :user
+    has_many :post_comments, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    
+    #shop_nameが存在しているかを確認するバリデーション
+    validates :shop_name, presence: true
+    #imageが存在しているかを確認するバリデーション
+    validates :image, presence: true
+    
     
     def get_image
         if image.attached?
@@ -19,6 +27,9 @@ class PostImage < ApplicationRecord
         image
     end
     
+    def favorited_by?(user)
+        favorites.exists?(user_id: user.id)
+    end
     
     
 end
